bdb52a24693259d9f12cc979c20e9f4df0455724
canodelacuadra <canodelacuadra@gmail.com>
2025-03-28 14:48:09 +0100

    hacemos fetch

---
+++ src/componentes/lista-peliculas/lista-peliculas.jsx
@@ -0,0 +1,48 @@
+
+import { useEffect, useState } from 'react';
+export default function ListaPeliculas() {
+  const [coleccion, setColeccion] = useState([]);
+  useEffect(() => {
+    fetch('./peliculas.json')
+      .then(res => res.json())
+      .then(data => setColeccion(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <>
+      <div className='container'>
+        <h1 className='display-1 text-center'>Catálogo de Películas</h1>
+        <div class="table-responsive">
+          <table className='table table-striped'>
+            <thead className='table-dark'>
+              <tr>
+                <th> Título</th>
+                <th> Director</th>
+                <th> Géneros</th>
+                <th> Año</th>
+              </tr>
+            </thead>
+            <tbody>
+              {coleccion.map((pelicula) => (
+                <tr key={pelicula.id}>
+                  <td>{pelicula.titulo}</td>
+                  <td>{pelicula.director}</td>
+
+                  <td>{pelicula.generos.join(' - ')}</td>
+
+                  <td>{pelicula.año}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+
+
+
+      </div>
+    </>
+
+  );
+}
